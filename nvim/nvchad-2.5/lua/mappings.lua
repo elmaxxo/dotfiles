cc19fcc2e765842e8275ae84c875cb1d7cf331fb
Kaitmazian Maksim <kaitmazian.mo@phystech.edu>
2024-11-26 21:57:06 +0300

    new mappings

--- nvim/nvchad-2.5/lua/mappings.lua
+++ nvim/nvchad-2.5/lua/mappings.lua
@@ -8,6 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 
 -- closer escapes
 map("i", "jk", "<ESC>")
+map("v", "fd", "<ESC>")
 map("i", "<C-k>", "<ESC>")
 map("i", "<C-j>", "<ESC>")
 
@@ -23,12 +24,20 @@ map({"n", "v"}, "<C-h>", "0")
 map({"n", "v"}, "<C-j>", "8j")
 map({"n", "v"}, "<C-k>", "8k")
 
+-- remap K
+map('n', '<S-t>', vim.lsp.buf.hover)
+
 -- telescope
 local telescope = require("telescope.builtin")
 map("n", "<leader>b", "<cmd> Telescope buffers <CR>", { desc = "Find buffers" })
 map("n", "<leader>gr", function() telescope.lsp_references() end, { desc = "LSP references", remap=true })
+map("n", "<leader>lds", function() telescope.lsp_document_symbols() end, { desc = "Show symbols in the cucrent buffer", remap=true })
+map("n", "<leader>gi", function() telescope.lsp_implementations() end, { desc = "Go to symbol implementations"})
+map("n", "<leader>gd", function() telescope.lsp_definition() end, { desc = "Go to symbol definition"})
 
 map("n", "<leader>ls", "<leader>ld", { desc = "Show diagnostic" })
 
 map("v", "<S-Tab>", "<gv", { desc = "Unindent line" })
 map("v", "<Tab>", ">gv", { desc = "Indent line" })
+
+map('n', '<space>L', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = "Show diagnostic in a floating window", remap=true })
